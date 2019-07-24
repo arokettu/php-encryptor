555c394d95ae118c0a6acf96ffc7915562358f73
Anton Smirnov <sandfox@sandfox.me>
2019-07-24 03:05:56 +0300

    Add version

--- make-phar.php
+++ make-phar.php
@@ -8,6 +8,13 @@ if (!is_dir(__DIR__ . '/build')) {
     mkdir(__DIR__ . '/build');
 }
 
+$version = trim(`git describe --tags`);
+
+$versionFile = __DIR__ . '/src/version.php';
+
+$oldVersion = file_get_contents($versionFile);
+file_put_contents($versionFile, "<?php return '{$version}';" . PHP_EOL);
+
 $phar = new Compiler(__DIR__);
 
 $phar->addIndexFile('bin/encryptor');
@@ -25,3 +32,5 @@ $phar->addDirectory('vendor', [
 ]);
 
 $phar->compile(__DIR__ . '/build/encryptor.phar');
+
+file_put_contents($versionFile, $oldVersion);
