917972f25ce0668a0f13ca50ce2666a1411e2162
Anton Smirnov <sandfox@sandfox.me>
2025-10-03 00:49:17 +0300

    Add copyright declaration

--- tests/TestData.php
+++ tests/TestData.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @copyright 2019 Anton Smirnov
+ * @license MIT https://spdx.org/licenses/MIT.html
+ */
+
 declare(strict_types=1);
 
 // @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
